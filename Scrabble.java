33997086840938d0ac20d15f5ba013ab0620d6f7
NewtonBoy <>
2021-05-27 09:31:48 +0000

    De a poco comienzo a entender TDD

--- Scrabble.java
+++ Scrabble.java
@@ -1,9 +1,12 @@
 public interface Scrabble{
+  
   Scrabble intoThisScrabble(Scrabble Sc);
   StringScrabble toStringSc(StringScrabble S);
   BoolScrabble toBool(BoolScrabble B);
+  /*
   FloatScrabble toFloat(FloatScrabble F);
   IntScrabble toIntSc(IntScrabble I);
   BinaryScrabble toBinary(BinaryScrabble Bi);
-  @Override String toString();
+  */
+  String toString();
 }
